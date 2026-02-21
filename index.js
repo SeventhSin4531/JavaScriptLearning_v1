4992d3032fabba27162a900ff6007c7f6e34c530
Oleg Tarasov <alfariusomegon202020@mail.ru>
2026-02-21 19:09:44 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// const myName = "Oleg"
+// console.log(myName)
+// myName = "23"
+// console.log(myName)
+
+// const objA = {a: 10, b: true}
+// const copyOfA = objA
+// console.log(objA)
+// copyOfA.a = 20
+// objA.b = false
+// console.log(objA)
+// copyOfA.c = "huiu"
+// console.log(objA)
+// console.log(objA.c)
+
+// let a2 = 10
+// console.log(a2)
+// a3 = undefined
+// console.log(a3)
+
+function a(){
+    console.log("Chort")
+}
+a()
