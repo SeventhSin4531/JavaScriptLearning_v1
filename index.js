56fc51a5c5c92cec1bda86ec55c79778a7789ba0
Oleg Tarasov <alfariusomegon202020@mail.ru>
2026-02-21 19:49:09 +0400

    Рекомендуется всегда создавать объекты через const, чтобы не переприсвоить значение объекта

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 // myName = "23"
 // console.log(myName)
 
+/**************************************************************/
+
 // const objA = {a: 10, b: true}
 // const copyOfA = objA
 // console.log(objA)
@@ -13,10 +15,14 @@
 // console.log(objA)
 // console.log(objA.c)
 
+/**************************************************************/
+
 // let a2 = 10
 // console.log(a2)
 // a3 = undefined
 // console.log(a3)
+
+/**************************************************************/
 
 // function a(){
 //     console.log("Chort")
@@ -27,10 +33,28 @@
 
 // a()
 
-const a = () =>{
-    console.log("Chort")
-}
-a()
+/**************************************************************/
 
-a = 20
-a()
+// const a = () =>{
+//     console.log("Chort")
+// }
+// a()
+
+// a = 20
+// a()
+
+// first second third
+
+const myObj = {
+    first: "1",
+    second: "2",
+    third: "3"
+}
+
+console.log(myObj)
+
+myObj.first = 1
+myObj.second = 2
+myObj.third = 3
+
+console.log(myObj)
